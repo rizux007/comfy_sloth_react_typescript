6ea764d9b0820e15a6e8819525ed8292bc2b1629
rizux007 <eriksheney@gmail.com>
2024-05-14 15:13:50 +0000

    Fix with basename solve 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/comfy_sloth_react_typescript/",
   plugins: [react()],
-})
+});
