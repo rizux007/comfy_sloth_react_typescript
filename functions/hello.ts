0ac7528fd71a80736d0c9c46714294644a868230
rizux007 <eriksheney@gmail.com>
2024-06-03 11:16:24 +0000

    Payment options & reviews added

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,14 +1,6 @@
 import { Handler } from "@netlify/functions";
 
-interface Items {
-  id: number;
-  name: string;
-}
-const items: Items[] = [
-  { id: 1, name: "john" },
-  { id: 2, name: "susan" },
-];
-const handler: Handler = async (event, context) => {
+const handler: Handler = async () => {
   return {
     statusCode: 200,
     // body: JSON.stringify(items),
